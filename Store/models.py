7515af483a7434893ae88cb3fb69ee5c7152c040
Jomgarciaro <jomgarciaro@unal.edu.co>
2021-04-22 08:37:32 -0500

    Create a database model for the orders

--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    costumer_name = models.CharField("Name", max_length=80)
+    costumer_email = models.EmailField("Email", max_length=120)
+    costumer_mobile = models.CharField("Mobile", max_length=40)
+    status = models.CharField("Status", max_length=20, default="CREATED")
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+   
