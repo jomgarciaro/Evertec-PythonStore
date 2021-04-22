7611531d667ca04714f7c2ef4ed5ed296053a013
Jomgarciaro <jomgarciaro@unal.edu.co>
2021-04-22 18:54:25 -0500

    Register templates directories

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'OnlineStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(__file__).resolve().parent / 'templates',
+                Path(__file__).resolve().parent / 'Store' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
