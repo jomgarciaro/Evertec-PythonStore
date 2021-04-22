4bf55d04b95b812d9464f6491fd1c2aaf6e0aa1b
Jomgarciaro <jomgarciaro@unal.edu.co>
2021-04-22 10:52:22 -0500

    Create view for a new order

--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import Order_form
+from .models import Order
 
-# Create your views here.
+def new_order_view(request):
+
+    if request.method == "POST":
+        form = Order_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(order_resume_view, id=form.save().id)
+    else:
+        form = Order_form()
+
+    context = {"form": form}
+
+    return render(request, "tienda/new_order.html", context)
