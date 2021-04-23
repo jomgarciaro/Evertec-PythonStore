045f0e554409eb96966108c1cf705c33de9f1b92
Jomgarciaro <jomgarciaro@unal.edu.co>
2021-04-22 19:01:45 -0500

    Import views from Store app

--- OnlineStore/urls.py
+++ OnlineStore/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Store.views import new_order_view, order_summary_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
